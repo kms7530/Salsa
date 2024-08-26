5b1934da1611e1d48b0d3fb12de963aa43235f28
kms7530 <6489395+kms7530@users.noreply.github.com>
2024-08-26 02:32:03 +0000

    [Chore] Black Formatter 수정 반영

--- config_template.py
+++ config_template.py
@@ -18,9 +18,7 @@ class Config:
         "infer_ground_box": "/ground-box",
     }
 
-    PATH_BENTOML_SERVER_BASE_URL: str = (
-        f"{PREF_BENTOML_SERVER['protocol']}{PREF_BENTOML_SERVER['path_root']}:{PREF_BENTOML_SERVER['port']}"
-    )
+    PATH_BENTOML_SERVER_BASE_URL: str = f"{PREF_BENTOML_SERVER['protocol']}{PREF_BENTOML_SERVER['path_root']}:{PREF_BENTOML_SERVER['port']}"
     PATH_CACHE: str = ".cache"
 
     # DINO 설정.
