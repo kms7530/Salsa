27c459aceb9e899a40a42767aefb23ec1920328c
Minseok Kwak <kwak@minseok.me>
2024-08-26 09:44:33 +0900

    [Feature] FastAPI 관련 코드 제거

--- service.py
+++ service.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import Dict, Union
+from typing import Dict
 
 import bentoml
 import numpy as np
 import torch
 from decord import VideoReader, cpu
-from fastapi import Depends, FastAPI
 from longva.constants import IMAGE_TOKEN_INDEX
 from longva.mm_utils import process_images, tokenizer_image_token
 from longva.model.builder import load_pretrained_model
@@ -15,18 +14,12 @@ from PIL.Image import Image as PILImage
 from transformers import AutoModelForZeroShotObjectDetection, AutoProcessor
 
 from config import Config
-from preprocess.video import download_video
-
-# FastAPI 객체 생성.
-app = FastAPI()
-app.task_counter = 0  # 현재 동작중인 worker의 개수를 담는 변수
 
 
 @bentoml.service(
     resources={"cpu": "2"},
     traffic={"timeout": 30},
 )
-@bentoml.mount_asgi_app(app, path="/v1")
 class VisionLanguage:
     def __init__(self) -> None:
         """BentoML 서비스 구동을 위한 모델과 기타 객체 및 옵션 생성."""
@@ -172,7 +165,6 @@ class VisionLanguage:
     resources={"cpu": "1"},
     traffic={"timeout": 10},
 )
-@bentoml.mount_asgi_app(app, path="/v1")
 class DINO:
     def __init__(self) -> None:
         """Ground DINO의 serving을 위한 객체 생성 함수."""
@@ -212,37 +204,3 @@ class DINO:
         )
 
         return results
-
-
-@app.get("/api/health")
-def health_check(service: VisionLanguage = Depends(bentoml.get_current_service)):
-    """FastAPI 서비스 상태 확인용 API.
-
-    Returns:
-        str: OK.
-    """
-
-    return "OK"
-
-
-@app.get("/keywords")
-def health_check(
-    code: Union[str, None],
-    service: VisionLanguage = Depends(bentoml.get_current_service),
-) -> Dict:
-    """주어진 영상 코드를 통해 영상을 받아 키워드를 도출하는 API 함수.
-
-    Args:
-        code (Union[str, None]): YouTube 영상 코드.
-        service (VisionLanguage, optional): BentoML과 연결을 위한 인자. Defaults to Depends(bentoml.get_current_service).
-
-    Returns:
-        Dict: 영상의 키워드 결과값.
-    """
-
-    # Youtube 영상 받아오기.
-    download_video(code, Config.PATH_CACHE)
-
-    # TODO: 기타 pipeline 작성.
-
-    return "OK"
